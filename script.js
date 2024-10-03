46b566754cdc4a47e10c497459073f3eb2d3aff8
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2024-10-03 17:01:44 +0200

    Upped the difficulty

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let playerName = undefined;
 
 const name = () => {
     let name = prompt('Insert your name');
-    if (name == undefined || name == null) playerName = 'Mysterious Player'; 
+    if (name == undefined || name == null || name === '') playerName = 'Mysterious Player'; 
     else playerName = name; 
 };
 
@@ -25,6 +25,7 @@ const game = (() => {
     let board = new Array(9).fill(null);
     endGame = false;
     if (endGame == true) return;
+    const winningCombinations = [[0,4,8], [2,4,6], [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8]];
 
     const round = (cell, symbol) => {
         if (cell.classList.contains('filled') || endGame == true) return;
@@ -32,18 +33,40 @@ const game = (() => {
         cell.textContent = symbol;
         board[id] = symbol;
         cell.classList.add('filled');
-        checkWin(symbol);
+        checkGameEnd(symbol);
     }
 
-    const checkWin = (symbol) => {
-        const winningCombinations = [[0,4,8], [2,4,6], [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8]];
-        const filledCells = board.reduce((acc, cell, index) => {
-            if (cell == symbol) acc.push(index);
-            return acc;
-        }, [])
-        
+    const filledCells = (symbol) => board.reduce((acc, cell, index, array) => {
+        if (cell == symbol ) acc.push(index);
+        return acc;
+    }, [])
+
+    const checkMove = (symbol) => {
+        const userCells = filledCells(symbol);
+        console.log(userCells)
+        for (const comb of winningCombinations) {
+            const [a, b, c] = comb;
+            if (userCells.includes(a) && userCells.includes(b) && board[c] == null) {
+                console.log(c)
+                return c
+            } 
+            if (userCells.includes(b) && userCells.includes(c) && board[a] == null) {
+                console.log(a)
+                return a
+            }
+            if (userCells.includes(a) && userCells.includes(c) && board[b] == null) {
+                console.log(b)
+                return b
+            } 
+        }
+
+        return false;
+    }
+
+    const checkGameEnd = (symbol) => {
+        const moves = filledCells(symbol);
         for (comb of winningCombinations) {
-            if (filledCells.includes(comb[0]) && filledCells.includes(comb[1]) && filledCells.includes(comb[2])) {
+            if (moves.includes(comb[0]) && moves.includes(comb[1]) && moves.includes(comb[2])) {
                 if (symbol == 'X') h1.textContent = 'You won! :)';
                 if (symbol == 'O') h1.textContent = 'You lost! :(';
                 endGame = true;
@@ -73,10 +96,19 @@ const game = (() => {
     }
 
     const computerChoice = () => {
-        const choice = (Math.floor(Math.random() * 9));
+        let choice = checkMove('O') || checkMove('X') || getRandomCell();
         const targetCell = document.getElementById(`${choice}`);
-        if (targetCell.classList.contains('filled')) computerChoice();
-        else round(targetCell, 'O');
+        round(targetCell, 'O');
+    }
+
+    const getRandomCell = () => {
+        const emptyCells = filledCells(null);
+        console.log(emptyCells)
+        const randomIndex = Math.floor(Math.random() * (emptyCells.length - 1))
+        console.log(randomIndex)
+        const randomCell = emptyCells[randomIndex];
+        console.log(randomCell);
+        return randomCell;
     }
 
     return {round, resetGame};
