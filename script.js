590fbf7ecdb928ac8baa3e2a254799cbb80cddd5
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2024-10-03 13:26:26 +0200

    First draft game mechanics

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
 const li = document.querySelectorAll('li');
 let board = new Array(9).fill(null);
+winningCombinations = [[0,4,8], [2,4,6], [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8]];
 
 li.forEach((item) => {
-    item.addEventListener('click', (e) => {
-        const id = +e.target.id;
-        console.log(id)
-        board[id] = e.target.textContent;
-
+    item.addEventListener('click', function() {
+        game(this, 'X');
     })
 })
 
+
+function game(cell, symbol) {
+    if (cell.classList.contains('filled')) return;
+    const id = +cell.id;
+    cell.textContent = symbol;
+    board[id] = symbol;
+    cell.classList.add('filled');
+    console.log('done')
+    checkWin(symbol);
+}
+
+function checkWin(symbol) {
+    const filledCells = board.reduce((acc, cell, index) => {
+        if (cell == symbol) acc.push(index);
+        return acc;
+    }, [])
+    
+    for (comb of winningCombinations) {
+        if (filledCells.includes(comb[0]) && filledCells.includes(comb[1]) && filledCells.includes(comb[2])) {
+            console.log('Yes');
+            resetGame();
+        }
+    }
+}
+
+function resetGame() {
+    li.forEach((item) => {
+        item.classList.remove('filled');
+        item.textContent = '';
+    })
+}
